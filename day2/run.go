7288a7bd59b844813f01d766481009253810569c
j.deklerk <jeroen.de.klerk@basgroup.com>
2024-12-04 13:21:49 +0100

    solved day2 part 1

--- day2/run.go
+++ day2/run.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"slices"
@@ -33,6 +34,36 @@ func readIntoDict(file *os.File) [][]int {
 	return reports
 }
 
+func isDescendingOrder(arr []int) bool {
+	return slices.IsSortedFunc(arr, func(a, b int) int {
+		if a < b {
+			return 1
+		} else if a > b {
+			return -1
+		} else {
+			return 0
+		}
+	})
+}
+
+func checkReport(report []int) bool {
+	for index, number := range report {
+		if index+1 >= len(report) {
+			continue
+		}
+
+		nextNum := report[index+1]
+
+		if (nextNum > number && (nextNum-number) > 3) ||
+			(nextNum < number && (number-nextNum) > 3) ||
+			(nextNum == number) {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	file, err := os.Open("./reports.txt")
 
@@ -42,8 +73,33 @@ func main() {
 	defer file.Close()
 
 	dict := readIntoDict(file)
+	safeReport := 0
 
 	for _, report := range dict {
+		originalReport := slices.Clone(report)
 		ascending := slices.IsSorted(report)
+		descending := isDescendingOrder(report)
+
+		if !ascending && !descending {
+			continue
+		}
+
+		unsafe := !checkReport(originalReport)
+
+		if unsafe {
+			fmt.Println(fmt.Errorf("report %s is unsafe", fmt.Sprint(originalReport)))
+			continue
+		}
+
+		safeReport++
 	}
+
+	// str, err := fmt.Printf("%s valid reports", string(safeReport))
+
+	// if err != nil {
+	// 	log.Fatal("it broke")
+	// }
+
+	// fmt.Println(str)
+	fmt.Println(safeReport)
 }
